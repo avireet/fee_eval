4b6e79061b582f7670377ef4dcf51d3346bf6d64
avireet <avireetkaur@yahoo.comgit config --global user.email avireetkaur@yahoo.com>
2024-05-05 00:17:08 +0530

    fee

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+import ContactPage from './components/ContactPage';
+import EduPackages from './components/packages';
+import Signin from './components/signin';
+import Signup from './components/signup';
+import Footer from './components/footer';
+import Page1 from './components/page1';
+import Quiz from './components/quiz';
+import Page3 from './components/page3';
+import Results from './components/result';
+import Vision from './components/vision';
+import Review from './components/review';
+import FAQ from './components/faq';
+import Com from './components/com';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/signup" component={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/packages" element={<EduPackages />} />
+        </Routes>
+        
+      </div>
+    </Router>
+  );
+}
+
+// Define a Home component if needed
+const Home = () => (
+  <>
+    <Page1 />
+    <Page3 />
+    <Quiz />
+    <Results />
+    <Vision />
+    <Review />
+    <FAQ />
+    <Com />
+    <Footer />
+  </>
+);
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import './styles.css'; // Import corresponding CSS file
+
+const Navbar = () => {
+  return (
+    <div className="page1">
+      <div id="navbar">
+        <a id="logo" href="#" style={{marginLeft: '10px'}}>Learnera</a>
+        <Link to="/home" style={{marginLeft: '600px'}}>Home</Link>
+        <Link to="/packages" >Packages</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to='/signin' style={{marginLeft: '550px', fontSize:'22px'}}>
+           <i className="fa-solid fa-user" ></i>
+         </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
+
